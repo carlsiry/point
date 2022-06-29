886312cfc89d9a7fb11d4b037b609c21eb71e630
chenkansheng <chenkansheng0@163.com>
2022-06-29 09:52:36 +0800

    refactor: casbin func test.

--- pkg/casbin/casbin_test.go
+++ pkg/casbin/casbin_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestModelRBAC_CheckPermission(t *testing.T) {
 
-	t.Run("valid user permission", func(t *testing.T) {
+	t.Run("validate user permission", func(t *testing.T) {
 
 		policies := Policies{
 			{
-				Subject: "role-bar-id", // 角色-bar
+				Subject: "role-bar-id", // role-bar
 				Object:  "dept-list",
-				Action:  "Get",
+				Action:  "get",
 			},
 		}
 
@@ -19,7 +19,7 @@ func TestModelRBAC_CheckPermission(t *testing.T) {
 		uid := "user-foo-1"
 		model.AddRoleForUser(uid, "role-bar-id")
 
-		ok, err := model.CheckPermission(uid, "dept-list", "Get")
+		ok, err := model.CheckPermission(uid, "dept-list", "get")
 		if err != nil || !ok {
 			t.Fail()
 		}
